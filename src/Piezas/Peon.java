c3baed8ed65a907be394b3efa8fc585edbb2d1f6
mende <mende@20.0.65.75>
2026-07-16 12:34:00 -0500

    Implementacion de funciones en la clase Peon para el uso de reglas en el juego

--- src/Piezas/Peon.java
+++ src/Piezas/Peon.java
@@ -4,6 +4,7 @@ import Tablero.Tablero;
 
 public class Peon extends Pieza {
 
+    //Constructor.
     public Peon(boolean blanca, int fila, int columna) {
 
         super(blanca, fila, columna);
@@ -13,7 +14,6 @@ public class Peon extends Pieza {
     @Override
     public String getSimbolo() {
 
-        // Usamos siempre el peón negro
         return "♟";
 
     }
@@ -22,6 +22,43 @@ public class Peon extends Pieza {
     public boolean movimientoValido(int filaDestino,
                                     int columnaDestino,
                                     Tablero tablero) {
+
+        int filaOrigen = getFila();
+        int columnaOrigen = getColumna();
+
+        // Debe permanecer en la misma columna para avanzar.
+        if (columnaOrigen != columnaDestino) {
+
+            return false;
+
+        }
+
+        // Dirección del movimiento.
+        int direccion = esBlanca() ? -1 : 1;
+
+        // Avanzar una casilla.
+        if (filaDestino == filaOrigen + direccion) {
+
+            return tablero.estaVacia(filaDestino, columnaDestino);
+
+        }
+
+        // Primer movimiento: puede avanzar dos casillas.
+        if (esBlanca() && filaOrigen == 6 &&
+                filaDestino == filaOrigen + (2 * direccion)) {
+
+            return tablero.estaVacia(filaOrigen + direccion, columnaOrigen)
+                    && tablero.estaVacia(filaDestino, columnaDestino);
+
+        }
+
+        if (!esBlanca() && filaOrigen == 1 &&
+                filaDestino == filaOrigen + (2 * direccion)) {
+
+            return tablero.estaVacia(filaOrigen + direccion, columnaOrigen)
+                    && tablero.estaVacia(filaDestino, columnaDestino);
+
+        }
 
         return false;
 
